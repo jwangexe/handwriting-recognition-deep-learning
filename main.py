8594238b9b58dedf8b5492e3a0a19a8f4c04617d
Jun Wang <wjun9189@gmail.com>
2023-02-19 08:39:39 +0000

    Edited deep learning code

---
+++ main.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+from tensorflow import keras
+
+# Load the MNIST dataset
+mnist = keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+# Preprocess the data
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+# Define the model
+model = keras.Sequential([
+    keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1)),
+    keras.layers.MaxPooling2D((2,2)),
+    keras.layers.Conv2D(64, (3,3), activation='relu'),
+    keras.layers.MaxPooling2D((2,2)),
+    keras.layers.Flatten(),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model
+model.fit(train_images.reshape(-1, 28, 28, 1), train_labels, epochs=5)
+
+# Evaluate the model on the test dataset
+test_loss, test_acc = model.evaluate(test_images.reshape(-1, 28, 28, 1), test_labels, verbose=2)
+print('Test accuracy:', test_acc)#
